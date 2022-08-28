d5ba4b996fa5631c907985254963f901d79945f5
Dennis Mullen <dkmullen@gmail.com>
2022-08-27 21:20:22 -0400

    pw field and button

--- vanilla-form/js/main.js
+++ vanilla-form/js/main.js
@@ -1,6 +1,7 @@
-let form = document.querySelector('#form');
+const form = document.querySelector('#form');
 let data = new FormData(form);
 let payload = {};
+
 form.addEventListener('submit', readForm);
 
 function readForm(event) {
@@ -9,4 +10,20 @@ function readForm(event) {
     payload[i[0]] = i[1];
   }
   console.log(payload);
+}
+
+const togglePwButton = document.querySelector('#toggle-pw-button');
+const pw = document.querySelector('#password');
+togglePwButton.addEventListener('click', togglePw);
+
+function togglePw(event) {
+  if (pw.type === 'password') {
+    pw.type = 'text';
+    document.querySelector('#visibility_on').hidden = true;
+    document.querySelector('#visibility_off').hidden = false;
+  } else {
+    pw.type = 'password';
+    document.querySelector('#visibility_on').hidden = false;
+    document.querySelector('#visibility_off').hidden = true;
+  }
 }
